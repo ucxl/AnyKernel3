b841a00d86f8fa77807def39eb6c37eadbe7f61c
ucxl <ucxl@qq.com>
2025-03-25 22:17:44 +0800

    Update anykernel.sh

--- anykernel.sh
+++ anykernel.sh
@@ -4,7 +4,7 @@
 ### AnyKernel setup
 # global properties
 properties() { '
-kernel.string=Z Fold/Flip 4-Knox-Disabled-Kernel-Kokuban
+kernel.string=taro kernel with KernelSU by Yufz
 do.devicecheck=1
 do.modules=0
 do.systemless=1
