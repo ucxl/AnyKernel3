c5c6a83266a58884b0611d4a6c99bb33331cff84
ucxl <ucxl@qq.com>
2025-03-25 22:20:38 +0800

    Update anykernel.sh

--- anykernel.sh
+++ anykernel.sh
@@ -4,7 +4,7 @@
 ### AnyKernel setup
 # global properties
 properties() { '
-kernel.string=S25-Knox-Disabled-Kernel-Kokuban
+kernel.string=sun kernel with KernelSU by Yufz
 do.devicecheck=1
 do.modules=1
 do.systemless=0
